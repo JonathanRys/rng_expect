5bebab8dff57fb6a6b46c08b295a7e07ba1fe8f9
bl-JonathanRys <jonathan.rys@betterlesson.com>
2022-05-03 17:20:04 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,67 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import InputForm from './InputForm';
+import { Chart as ChartJS } from 'chart.js/auto';
+import { Chart } from 'react-chartjs-2';
 
 function App() {
+  const chartRef = React.useRef(null);
+  const [labels, setLabels] = React.useState(["0", "5000", "10000"]);
+  const [maxValue, setMaxValue] = React.useState(0);
+  const [chartData, setChartData] = React.useState({
+    labels: labels,
+    datasets: [],
+    divisions: 10
+  });
+
+  React.useEffect(() => {
+    const chart = chartRef.current;
+
+    if (chart) {
+      chart.update();
+    }
+  }, [chartData]);
+
+  const options = {
+    scales: {
+      y: {
+        beginAtZero: true,
+        title: {
+          display: true,
+          text: '# of results for range'
+        }
+      },
+      x: {
+        beginAtZero: true,
+        position: 'right',
+        gridLines: {
+          offsetGridLines: true
+        },
+        title: {
+          display: true,
+          text: 'Number of tries'
+        }
+      }
+    },
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+          Find RNG Expectations
       </header>
+      <InputForm
+        chartData={chartData}
+        setChartData={setChartData}
+        setMaxValue={setMaxValue}
+      />
+      <div className="chart-container">
+        <Chart type='bar' data={chartData} options={options} />
+      </div>
+      <footer className="App-footer">
+        <div>{maxValue ? `Most number of tries: ${maxValue}` : ''}</div>
+      </footer>
     </div>
   );
 }
---
+++ src/InputForm.js
@@ -0,0 +1,137 @@
+import React from 'react';
+
+const InputForm = (props) => {
+  const [isLoading, setIsLoading] = React.useState(false);
+  const [reps, setReps] = React.useState(10000);
+  const [odds, setOdds] = React.useState(0.0001);
+
+  const calcRng = () => {
+      let count = 1;
+      while (Math.random() > odds) {
+        count++
+      };
+      return count;
+  }
+
+  const transformChartData = data => {
+    const maxValue = Math.max(...data);
+
+    const valStr = maxValue.toString();
+
+    const lowerBound = 0; // TODO: update code to account for this being different
+    const upperBound = parseInt( `${+valStr[0] + 1}${'0'.repeat( valStr.length - 1 )}`);
+
+    // make sure this divides evenly
+    //arrayLength = upperBound / props.chartData.divisions || 1;
+
+    // 0-fill the array and create labels
+    const chartData = [];
+    const labels = [];
+    const step = upperBound / props.chartData.divisions;
+
+    for (let i = 0 ; i < props.chartData.divisions ; i++) {
+      chartData[i] = [];
+      labels.push(`${i * step} - ${(i + 1) * step - 1}`);
+    }
+
+    // Count data
+    data.forEach(value => {
+      const arrayIndex = Math.floor(value / upperBound * props.chartData.divisions);
+      chartData[arrayIndex].push(value);
+    });
+
+    return {
+      labels: labels,
+      datasets: [{
+        label: 'RNG data frequency',
+        backgroundColor: 'rgb(132, 99, 255)',
+        borderColor: 'rgb(132, 99, 255)',
+        data: chartData.map(value => value.length),
+      }],
+      divisions: props.chartData.divisions,
+      options: {
+        scales: {
+          xAxes: {
+            gridLines: {
+              offsetGridLines: false
+            }
+          }
+        }
+      }
+    }
+  };
+
+  const findBestOf = (f, iterations) => {
+    let max = 0;
+    let results = [];
+
+    for (let i = 0 ; i < iterations ; i++) {
+        const result = f();
+        results.push(result);
+        max = max < result ? result : max;
+    }
+
+    results.sort((a, b) => a - b)
+
+    return {
+      max: max,
+      count: results.length,
+      total: results.reduce((acc, a) => acc + a, 0),
+      average: results.reduce((acc, a) => acc + a, 0) / results.length,
+      chartData: results
+    };
+  }
+
+  React.useEffect(() => {
+    if (isLoading) {
+      const result = findBestOf(calcRng, reps);
+      console.log('result:', result)
+      props.setChartData(transformChartData(result.chartData));
+      props.setMaxValue(result.max);
+      setIsLoading(false);
+    }
+  });
+
+  const handleClick = () => {
+    setIsLoading(true);
+  }
+
+  return (
+    <div className="input-form">
+        <div>
+            <label>
+              Number of repititions:
+            </label>
+            <input
+              type="number"
+              max="1000000"
+              value={reps}
+              onChange={(e) => setReps(e.target.value)}
+            />
+        </div>
+        <div>
+            <label>
+              Odds:
+            </label>
+            <input
+              type="number"
+              max="1"
+              precision="0.0001"
+              step="0.0001"
+              value={odds}
+              onChange={(e) => setOdds(e.target.value)}
+            />
+        </div>
+        <div>
+          <input
+            type="button"
+            onClick={handleClick}
+            disabled={isLoading}
+            value={isLoading ? "Loading..." : "Calculate"}
+          />
+        </div>
+    </div>
+  );
+}
+
+export default InputForm;
